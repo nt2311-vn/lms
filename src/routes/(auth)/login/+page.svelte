75ae596eb63ad49312820857449ee488ca678070
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-19 00:03:28 +0700

    group auth route

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import * as Form from '$lib/components/ui/form';
+	import Input from 'postcss/lib/input';
+</script>
+
+<div>
+	<h1 class="py-5 text-center text-2xl font-semibold">Welcome</h1>
+	<!-- form -->
+	<form method="POST" use:enhance>
+		<Form.Field {form} name="email">
+			<Form.Control let:attrs>
+				<Form.Label>Email</Form.Label>
+				<Input {...attrs} bind:value={$formData.email} />
+			</Form.Control>
+			<Form.Description />
+			<Form.FieldErrors />
+		</Form.Field>
+	</form>
+</div>
