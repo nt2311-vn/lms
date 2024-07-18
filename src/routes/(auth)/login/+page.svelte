4081f416040dbe6b690f0037aaba8434f9b1097f
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-19 01:08:37 +0700

    servere render page on frontend of login

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -2,6 +2,7 @@
 	import * as Form from '$lib/components/ui/form';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import { loginSchema } from '$lib/schema';
+	import { Loader2 } from 'lucide-svelte';
 	import { superForm } from 'sveltekit-superforms';
 	import { zodClient } from 'sveltekit-superforms/adapters';
 
@@ -14,7 +15,7 @@
 <div>
 	<h1 class="py-5 text-center text-2xl font-semibold">Welcome</h1>
 	<!-- form -->
-	<form method="POST" use:enhance>
+	<form method="POST" action="/" use:enhance>
 		<Form.Field {form} name="email">
 			<Form.Control let:attrs>
 				<Form.Label>Email</Form.Label>
@@ -31,5 +32,18 @@
 			<Form.Description />
 			<Form.FieldErrors />
 		</Form.Field>
+		<Form.Button class="w-full">
+			{#if $delayed}
+				<Loader2 class="size-6 animate-spin" />
+			{:else}
+				Login
+			{/if}
+		</Form.Button>
+		<div class="my-4 flex items-center">
+			<div class="flex-grow border-t border-gray-500"></div>
+			<div class="mx-4 text-gray-700">OR</div>
+			<div class="flex-grow border-t border-gray-500"></div>
+		</div>
+		<Form.Button variant="secondary" href="/register" class="w-full">Register</Form.Button>
 	</form>
 </div>
