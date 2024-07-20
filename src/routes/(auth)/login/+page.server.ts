0423f9dbf99025dfe3e3102b2b1c23dbbe06af7d
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-20 15:58:50 +0700

    login function with pocketbase auth

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,8 +1,35 @@
+import PocketBase, { ClientResponseError } from 'pocketbase';
 import { loginSchema } from '@/schema';
-import { superValidate } from 'sveltekit-superforms';
+import { fail, message, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import type { PageServerLoad } from './$types';
+import { redirect, type Actions } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async () => {
 	return { form: await superValidate(zod(loginSchema)) };
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		const {
+			locals: { pb }
+		}: { locals: { pb: PocketBase } } = event;
+		const form = await superValidate(event, zod(loginSchema));
+
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+
+		try {
+			await pb.collection('users').authWithPassword(form.data.email, form.data.password);
+		} catch (err) {
+			const status = err as ClientResponseError;
+
+			return message(form, { status, message: `Error occured ${err}` });
+		}
+
+		redirect(303, '/');
+	}
 };
